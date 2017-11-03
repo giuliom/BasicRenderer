40f26ff8d049630842143e77a9d7f694f1ee0bd3
Giulio  M <1172898+giuliom@users.noreply.github.com>
2017-11-03 13:40:39 +0100

    frame buffer optimizations

--- BasicRenderer/FrameBuffer.cpp
+++ BasicRenderer/FrameBuffer.cpp
@@ -16,10 +16,24 @@ bool FrameBuffer::WriteToColor(int w, int h, const Color& col)
 	return true;
 }
 
+bool FrameBuffer::WriteToColor(int index, const Color & col)
+{
+	if (index < 0 || index >= size) return false;
+	colorBuffer[index] = col;
+	return true;
+}
+
 bool FrameBuffer::WriteToDepth(int w, int h, float col)
 {
 	if (w < 0 || h < 0 || w >= width || h >= height) return false;
 	depthBuffer[width * h + w] = col;
+	return true;
+}
+
+bool FrameBuffer::WriteToDepth(int index, float col)
+{
+	if (index < 0 || index >= size) return false;
+	depthBuffer[index] = col;
 	return true;
 }
 
@@ -28,9 +42,19 @@ float FrameBuffer::GetDepth(int w, int h) const
 	return depthBuffer[width * h + w];
 }
 
+float FrameBuffer::GetDepth(int index) const
+{
+	return depthBuffer[index];
+}
+
 const Color& FrameBuffer::GetColor(int w, int h) const
 {
 	return colorBuffer[width * h + w];
+}
+
+const Color & FrameBuffer::GetColor(int index) const
+{
+	return colorBuffer[index];
 }
 
 void FrameBuffer::Fill(const Color& col)
@@ -38,7 +62,7 @@ void FrameBuffer::Fill(const Color& col)
 	float x = col.x;
 	float y = col.y;
 	float z = col.z;
-	for (int i = 0; i < width * height; ++i)
+	for (int i = 0; i < size; ++i)
 	{
 		colorBuffer[i].x = x;
 		colorBuffer[i].y = y;
--- BasicRenderer/FrameBuffer.h
+++ BasicRenderer/FrameBuffer.h
@@ -9,6 +9,7 @@ class FrameBuffer
 protected:
 	int width = 640;
 	int height = 480;
+	int size = 640 * 480;
 
 	Color* colorBuffer = nullptr;
 	float* depthBuffer = nullptr;
@@ -19,14 +20,19 @@ public:
 	{
 		colorBuffer = new Color[width_ * height_];
 		depthBuffer = new float[width_ * height_];
+		size = width_ * height_;
 	}
 	~FrameBuffer();
 
 	bool WriteToColor(int w, int h, const Color& col);
+	bool WriteToColor(int index, const Color& col);
 	bool WriteToDepth(int w, int h, float col);
+	bool WriteToDepth(int index, float col);
 
 	float GetDepth(int w, int h) const;
+	float GetDepth(int index) const;
 	const Color& GetColor(int w, int h) const;
+	const Color& GetColor(int index) const;
 
 	void Fill(const Color& col);
 	Color* GetBufferCopy() const;
