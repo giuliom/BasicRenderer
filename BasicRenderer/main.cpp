6db90244759ec35e7de688fbef8436dfd0fb73c3
Giulio  M <1172898+giuliom@users.noreply.github.com>
2017-12-04 22:58:30 +0100

    Added cmd options to BasicRenderer

--- BasicRenderer/main.cpp
+++ BasicRenderer/main.cpp
@@ -9,16 +9,84 @@
 
 int main(int argc, char *argv[])
 {
-
-	const char* file = "C:/BasicRenderer/Assets/bunny.obj"; //TODO "../../Assets/bunny.obj";
-
-	if (argc > 1) file = argv[1];
+	std::string file = "C:/BasicRenderer/Assets/bunny.obj"; //TODO "../../Assets/bunny.obj";
 
 	int width = 1920;
 	int height = 1080;
 
+	std::string outputFile = "./brender.bmp";
+
+	std::string renderingMode = "TODOr";
+	std::string shadingMode = "TODOs";
+
+
+	if (argc == 2)
+	{
+		if (std::strcmp(argv[1], "help") == 0)
+		{
+		std::cout << "The following options are available:" << std::endl << std::endl
+			<< "-n output image name and path" << std::endl
+			<< "-f input obj file name and path" << std::endl
+			<< "-w output image width" << std::endl
+			<< "-h output image height" << std::endl
+			<< "-r rendering mode" << std::endl
+			<< "-s shading mode" << std::endl
+			<< std::endl;
+		}
+		else
+		{
+			std::cout << "Digit 'help' to show the available options" << std::endl;
+		}	
+		return 0;
+	}
+
+	for (int an = 1; an < argc -1;)
+	{
+		if (std::strcmp(argv[an], "-w") == 0)
+		{
+			width = atoi(argv[an + 1]);
+		}
+		else if (std::strcmp(argv[an], "-h") == 0)
+		{
+			height = atoi(argv[an + 1]);
+		}
+		else if (std::strcmp(argv[an], "-n") == 0)
+		{
+			outputFile = argv[an + 1];
+		}
+		else if (std::strcmp(argv[an], "-r") == 0)
+		{
+			renderingMode = argv[an + 1];
+		}
+		else if (std::strcmp(argv[an], "-s") == 0)
+		{
+			shadingMode = argv[an + 1];
+		}
+		else if (std::strcmp(argv[an], "-f") == 0)
+		{
+			file = argv[an + 1];
+		}
+		else
+		{
+			if (std::strcmp(argv[an], "help") != 0) std::cout << std::string(argv[an]) + " is not valid.";
+			std::cout<< "Digit 'help' to show the available options" << std::endl;
+			return 0;
+		}
+
+		an += 2;
+	}
+
+	std::cout << "Output: " + outputFile<< std::endl;
+	std::cout << "Input: " + file<< std::endl;
+	std::cout << "Width: " << width<< std::endl;
+	std::cout << "Height: " << height << std::endl;
+	std::cout << "Rendering: " + renderingMode<< std::endl;
+	std::cout << "Shading: " + shadingMode << std::endl;
+	std::cout << "----- RENDERING -----"<<std::endl;
+
+	
 	BasicRenderer renderer;
-	SceneObject bunny(ObjLoader::Load(file));
+	SceneObject bunny(ObjLoader::Load(file.c_str()));
 
 	auto beginClock = clock();
 	
