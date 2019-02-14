f6c3170f0b79451a6611491745feaca471e578e9
Giulio M <1172898+giuliom@users.noreply.github.com>
2019-02-14 18:27:46 +0100

    Update Face.h

--- BasicRenderer/Face.h
+++ BasicRenderer/Face.h
@@ -35,9 +35,9 @@ public:
 
 	inline Face ToMatrixSpace(const Matrix4& m) const
 	{
-		return Face(Vertex(m * v0.pos, v0.nrml, v0.uv),
-					Vertex(m * v1.pos, v1.nrml, v1.uv),
-					Vertex(m * v2.pos, v2.nrml, v2.uv));
+		return Face(Vertex(m * v0.pos, m * v0.nrml, v0.uv),
+					Vertex(m * v1.pos, m * v1.nrml, v1.uv),
+					Vertex(m * v2.pos, m * v2.nrml, v2.uv));
 	}
 
 	Vector3 CalculateNormal() const;
