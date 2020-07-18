4c1c99c3e6764a7cda9754dbb26c2fcb47d7f8d0
Giulio M <1172898+giuliom@users.noreply.github.com>
2020-07-18 18:48:35 +0100

    Update Ray.h
    removed useless todo

--- BasicRenderer/Ray.h
+++ BasicRenderer/Ray.h
@@ -15,7 +15,5 @@ namespace BasicRenderer
 		Ray(const Vector3& ori, const Vector3& dir) : origin(ori), direction(dir.Normalize()) {}
 
 		inline Vector3 GetPoint(float t) const { return origin + direction * t; }
-
-		//TODO move semantics
 	};
 }
