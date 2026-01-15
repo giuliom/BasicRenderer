725964390dc5a343e1ffe243f83eb4d107b92827
Giulio M <1172898+giuliom@users.noreply.github.com>
2026-01-15 22:25:19 +0000

    Improved script support for build/release configurations

--- BasicRenderer/build.sh
+++ BasicRenderer/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "Building BasicRenderer"
-cmake -S . -B build
-cmake --build build --config release --target BasicRenderer
+# Default to Release if no argument provided
+BUILD_TYPE=${1:-Release}
+
+# Validate build type
+if [[ "$BUILD_TYPE" != "Debug" && "$BUILD_TYPE" != "Release" ]]; then
+    echo "Usage: ./build.sh [Debug|Release]"
+    echo "  Default: Release"
+    exit 1
+fi
+
+echo "Building BasicRenderer ($BUILD_TYPE)"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake --build build --config $BUILD_TYPE --target BasicRenderer
