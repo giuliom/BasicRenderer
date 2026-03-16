efdd8714c9d17334b3d422cd1b36b4764df79330
Giulio M <1172898+giuliom@users.noreply.github.com>
2026-03-16 21:52:32 +0000

    Use of std::filesystem for paths

--- BasicRenderer/source/PathUtils.cpp
+++ BasicRenderer/source/PathUtils.cpp
@@ -1,5 +1,7 @@
 #include "PathUtils.h"
 
+#include <filesystem>
+
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -9,6 +11,8 @@
 #include <mach-o/dyld.h>
 #endif
 #endif
+
+namespace fs = std::filesystem;
 
 namespace BasicRenderer
 {
@@ -20,18 +24,11 @@ namespace BasicRenderer
 		char buffer[MAX_PATH];
 		GetModuleFileNameA(NULL, buffer, MAX_PATH);
 		execPath = buffer;
-		// Find last backslash
-		size_t pos = execPath.find_last_of("\\/");
-		if (pos != std::string::npos)
-		{
-			execPath = execPath.substr(0, pos);
-		}
 #elif __APPLE__
 		char buffer[PATH_MAX];
 		uint32_t size = sizeof(buffer);
 		if (_NSGetExecutablePath(buffer, &size) == 0)
 		{
-			// Resolve any symlinks
 			char realPath[PATH_MAX];
 			if (realpath(buffer, realPath) != nullptr)
 			{
@@ -41,12 +38,6 @@ namespace BasicRenderer
 			{
 				execPath = buffer;
 			}
-			// Find last slash
-			size_t pos = execPath.find_last_of('/');
-			if (pos != std::string::npos)
-			{
-				execPath = execPath.substr(0, pos);
-			}
 		}
 #else  // Linux
 		char buffer[PATH_MAX];
@@ -55,52 +46,22 @@ namespace BasicRenderer
 		{
 			buffer[len] = '\0';
 			execPath = buffer;
-			// Find last slash
-			size_t pos = execPath.find_last_of('/');
-			if (pos != std::string::npos)
-			{
-				execPath = execPath.substr(0, pos);
-			}
 		}
 #endif
-		return execPath;
+		return fs::path(execPath).parent_path().string();
 	}
 
 	std::string GetProjectRootDirectory()
 	{
-        // It's assumed to be in ${PROJECT_ROOT}/build/bin
-		std::string execDir = GetExecutableDirectory();
-		
-#ifdef _WIN32
-		const char separator = '\\';
-#else
-		const char separator = '/';
-#endif
-
-		size_t pos = execDir.find_last_of("/\\");
-		if (pos != std::string::npos)
-		{
-			execDir = execDir.substr(0, pos);
-		}
-
-		pos = execDir.find_last_of("/\\");
-		if (pos != std::string::npos)
-		{
-			execDir = execDir.substr(0, pos);
-		}
-
-		return execDir;
+		// It's assumed to be in ${PROJECT_ROOT}/build/bin
+		fs::path execDir = GetExecutableDirectory();
+		return execDir.parent_path().parent_path().string();
 	}
 
 	std::string GetAssetPath(const std::string& relativePath)
 	{
-        // It's assumed to be in ${PROJECT_ROOT}/assets
-		std::string root = GetProjectRootDirectory();
-		
-#ifdef _WIN32
-		return root + "\\assets\\" + relativePath;
-#else
-		return root + "/assets/" + relativePath;
-#endif
+		// It's assumed to be in ${PROJECT_ROOT}/assets
+		fs::path assetPath = fs::path(GetProjectRootDirectory()) / "assets" / relativePath;
+		return assetPath.string();
 	}
 }
